0f270318bf118154ae7484a85fb8a2ffc3e3fa79
devman0129 <oleksandrkliatskyi.dev@outlook.com>
2023-12-27 02:36:11 -0500

    modified main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from fastapi import Request, FastAPI, Response
+
+app = FastAPI(title = "S2DR3")
+
+AIP_HEALTH_ROUTE = os.environ.get("AIP_HEALTH_ROUTE", "/health")
+AIP_PREDICT_ROUTE = os.environ.get("AIP_PREDICT_ROUTE", "/predict")
+
+@app.get(AIP_HEALTH_ROUTE, status_code = 200)
+async def health():
+    return {"health": "ok"}
+
+@app.post(AIP_PREDICT_ROUTE)
+async def predict(request: Request):
+    body = await request.json()
+    print(body)
+
+    instances = body["instances"]
+    print(instances)
+
+    return instances
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port = 8080)
